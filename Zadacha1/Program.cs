5fabd8b467f05cb3ec7c2a08ba35586154ee700d
Aleksandra <s-bass11@yandex.ru>
2022-06-13 00:49:47 +0400

    Zadacha

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void NumbersFromMtoN (int M, int N)
+{
+    Console.Write(M + " ");
+    if (M==N) return;
+    M++;
+    NumbersFromMtoN(M,N);
+}
+NumbersFromMtoN(1,20);
+
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы.
+int SumElements (int m, int n)
+{ 
+    if(m == n) return n;
+    else return m + SumElements(m+1, n); 
+}
+Console.WriteLine("Сумма элементов от M до N равна: ");
+Console.Write(SumElements(1, 10));
